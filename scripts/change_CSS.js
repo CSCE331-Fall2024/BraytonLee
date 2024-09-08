ea2a3ff4da0fd0f9a9bd1a1443f4adb4e3fe9998
Brayton Lee <sicou2@gmail.com>
2024-09-07 22:17:16 -0500

    creates .js for switching css on the page

---
+++ scripts/change_CSS.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("currentStylesheet") === null) {
+        document.getElementById("stylesheet_id").href = "styles/spaceStyle.css"
+        localStorage.setItem("currentStylesheet", "proStyle")
+    } else {
+        document.getElementById("stylesheet_id").href = localStorage.getItem("currentStylesheet")
+    }
+})
+
+// if (localStorage.getItem("currentStylesheet") === null) {
+//     document.getElementById("stylesheet_id").href = "styles/spaceStyle.css"
+//     localStorage.setItem("currentStylesheet", "proStyle")
+// }
+
+
+function theme1Link() {
+    document.getElementById("stylesheet_id").href = "styles/spaceStyle.css"
+    localStorage.setItem("currentStylesheet", "styles/spaceStyle.css")
+}
+
+function theme2Link() {
+    document.getElementById("stylesheet_id").href = "styles/proStyle.css"
+    localStorage.setItem("currentStylesheet", "styles/proStyle.css")
+}
