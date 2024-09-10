01e6ddf59df041db80363e56c5558a3102027913
Brayton Lee <sicou2@gmail.com>
2024-09-09 20:09:20 -0500

    reorders functions and changes default theme

--- scripts/change_CSS.js
+++ scripts/change_CSS.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     if (localStorage.getItem("currentStylesheet") === null) {
-        document.getElementById("stylesheet_id").href = "styles/spaceStyle.css"
+        document.getElementById("stylesheet_id").href = "styles/proStyle.css"
         localStorage.setItem("currentStylesheet", "proStyle")
     } else {
         document.getElementById("stylesheet_id").href = localStorage.getItem("currentStylesheet")
@@ -14,11 +14,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function theme1Link() {
-    document.getElementById("stylesheet_id").href = "styles/spaceStyle.css"
-    localStorage.setItem("currentStylesheet", "styles/spaceStyle.css")
+    document.getElementById("stylesheet_id").href = "styles/proStyle.css"
+    localStorage.setItem("currentStylesheet", "styles/proStyle.css")
 }
 
 function theme2Link() {
-    document.getElementById("stylesheet_id").href = "styles/proStyle.css"
-    localStorage.setItem("currentStylesheet", "styles/proStyle.css")
+    document.getElementById("stylesheet_id").href = "styles/spaceStyle.css"
+    localStorage.setItem("currentStylesheet", "styles/spaceStyle.css")
 }
